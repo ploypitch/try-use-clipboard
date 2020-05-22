9b483b93ead3ceea7c0be47763576b6596c3659b
Pitchapa Pawong <ploypitch3338@gmail.com>
2020-05-22 19:09:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+import Button from "@material-ui/core/Button";
+import { useClipboard } from "use-clipboard-copy";
 
-function App() {
+const Box = styled.div`
+  width: 22rem;
+  height: 12rem;
+  background: #eaf6ff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1rem;
+  margin: 2rem;
+  color: rgba(0, 0, 0, 0.6);
+  font-size: 0.88rem;
+  line-height: 2rem;
+`;
+const CopyButton = styled(Button)`
+  && {
+    width: 18rem;
+    height: 3rem;
+    background: #5cdccd;
+    color: white;
+    font-family: "Courier New";
+    font-weight: 700;
+    font-size: 1rem;
+    text-transform: none;
+    margin-top: 1rem;
+    &:hover {
+      background: #48b0a4;
+    }
+  }
+`;
+const StyledInput = styled.input`
+  && {
+    width: 18rem;
+    height: 3rem;
+    background: #dceefc;
+    color: #5cdccd;
+    border: none;
+    border-radius: 0.25rem;
+    font-family: "Courier New";
+    font-weight: 700;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+`;
+const FlexRow = styled.div`
+  display: flex;
+`;
+
+export default function App() {
+  const clipboard = useClipboard({ copiedTimeout: 900 });
+  const url = "verme.me";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <FlexRow>
+          <Box>
+            Copying Text of Another Target Element
+            <StyledInput ref={clipboard.target} value={url} readOnly />
+            <CopyButton onClick={clipboard.copy}>Copy to Clipboard</CopyButton>
+          </Box>
+          <Box>
+            Copying Text Imperatively (Without a Target Element)
+            <CopyButton onClick={() => clipboard.copy(new Date().toString())}>
+              Copy to Clipboard
+            </CopyButton>
+          </Box>
+        </FlexRow>
+        <FlexRow>
+          <Box>
+            Check Copied
+            <StyledInput ref={clipboard.target} value={url} readOnly />
+            <CopyButton onClick={clipboard.copy}>
+              {clipboard.copied ? "Copied to Clipboard!" : "Copy to Clipboard"}
+            </CopyButton>
+          </Box>
+          <Box>
+            Check Browser Support
+            <StyledInput ref={clipboard.target} value={url} readOnly />
+            <CopyButton onClick={clipboard.copy}>
+              {clipboard.copied ? "Copied to Clipboard!" : "Copy to Clipboard"}
+            </CopyButton>
+            {clipboard.isSupported()
+              ? "(Supported Browser)"
+              : "(Unsupported Browser)"}
+          </Box>
+        </FlexRow>
+      </div>
     </div>
   );
 }
-
-export default App;
